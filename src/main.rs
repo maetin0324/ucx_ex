8e8c5a5f0d09e1f68d9fe6ee38d58c5ffb25c99c
maetin0324 <maetin0324@gmail.com>
2024-10-18 18:08:06 +0900

    hello

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+const HELLO: &str = "Hello!";
+
 async fn client(server_addr: String) -> Result<()> {
     println!("client: connect to {:?}", server_addr);
     let context = Context::new().unwrap();
     let worker = context.create_worker().unwrap();
-
     #[cfg(not(feature = "event"))]
     tokio::task::spawn_local(worker.clone().polling());
     #[cfg(feature = "event")]
@@ -28,9 +29,12 @@ async fn client(server_addr: String) -> Result<()> {
         .connect_socket(server_addr.parse().unwrap())
         .await
         .unwrap();
-    endpoint.print_to_stderr();
 
-    endpoint.stream_send(b"Hello!").await.unwrap();
+    println!("send: {:?}", HELLO);
+    endpoint.tag_send(100, HELLO.as_bytes()).await.unwrap();
+
+    let long_msg: Vec<u8> = (0..0x1000).map(|x| x as u8).collect();
+    endpoint.tag_send(101, &long_msg).await.unwrap();
     Ok(())
 }
 
@@ -38,7 +42,6 @@ async fn server() -> Result<()> {
     println!("server");
     let context = Context::new().unwrap();
     let worker = context.create_worker().unwrap();
-
     #[cfg(not(feature = "event"))]
     tokio::task::spawn_local(worker.clone().polling());
     #[cfg(feature = "event")]
@@ -49,13 +52,19 @@ async fn server() -> Result<()> {
         .unwrap();
     println!("listening on {}", listener.socket_addr().unwrap());
     let connection = listener.next().await;
-    let endpoint = worker.accept(connection).await.unwrap();
+    let _endpoint = worker.accept(connection).await.unwrap();
     println!("accept");
-    endpoint.print_to_stderr();
 
-    let mut buf = [MaybeUninit::uninit(); 10];
-    let len = endpoint.stream_recv(&mut buf).await.unwrap();
-    let msg = std::str::from_utf8(unsafe { transmute(&buf[..len]) });
+    let mut buf = [MaybeUninit::uninit(); 0x1005];
+    let len = worker.tag_recv(100, &mut buf).await.unwrap();
+    let msg = std::str::from_utf8(unsafe { transmute(&buf[..len]) }).unwrap();
     println!("recv: {:?}", msg);
+    assert_eq!(msg, HELLO);
+
+    let len = worker.tag_recv(101, &mut buf).await.unwrap();
+    println!("recv long message, len={}", len);
+    let msg: &[u8] = unsafe { transmute(&buf[..len]) };
+    let long_msg: Vec<u8> = (0..0x1000).map(|x| x as u8).collect();
+    assert_eq!(msg, long_msg.as_slice());
     Ok(())
 }
